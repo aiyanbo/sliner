cbf6738b4480dd2a600581fcfe857c31c2cac607
Andy Ai <yanbo.ai@gmail.com>
2014-02-11 16:10:18 +0800

    Handle null point exception

--- src/main/java/org/sliner/impl/SqlLinerImpl.java
+++ src/main/java/org/sliner/impl/SqlLinerImpl.java
@@ -25,36 +25,40 @@ public class SqlLinerImpl implements SqlLiner {
     @Override
     public SqlWrapper wrap(String key, Map<String, String> conditions, String operator, List<String> sorters) {
         StringBuilder sqlBuilder = new StringBuilder("select * from ");
-        sqlBuilder.append(selectionGenerator.generateEntityName(key));
-        List<Condition> _conditions = selectionGenerator.generateConditions(key, conditions);
         SqlWrapper sqlWrapper = new SqlWrapper();
-        if (CollectionUtilities.isNotEmpty(_conditions)) {
-            sqlBuilder.append(" WHERE ");
-            List<String> lines = new ArrayList<String>(_conditions.size());
-            List<String> names = new ArrayList<String>(_conditions.size());
-            List<Object> values = new ArrayList<Object>(_conditions.size());
-            for (Condition condition : _conditions) {
-                StringBuilder lineBuilder = new StringBuilder();
-                lineBuilder.append(condition.getColumnName());
-                lineBuilder.append(" ");
-                lineBuilder.append(condition.getOperator().getOperator());
-                lineBuilder.append(" ?");
-                lines.add(lineBuilder.toString());
-                names.add(condition.getName());
-                values.add(condition.getValue());
+        sqlBuilder.append(selectionGenerator.generateEntityName(key));
+        if (CollectionUtilities.isNotEmpty(conditions)) {
+            List<Condition> _conditions = selectionGenerator.generateConditions(key, conditions);
+            if (CollectionUtilities.isNotEmpty(_conditions)) {
+                sqlBuilder.append(" WHERE ");
+                List<String> lines = new ArrayList<String>(_conditions.size());
+                List<String> names = new ArrayList<String>(_conditions.size());
+                List<Object> values = new ArrayList<Object>(_conditions.size());
+                for (Condition condition : _conditions) {
+                    StringBuilder lineBuilder = new StringBuilder();
+                    lineBuilder.append(condition.getColumnName());
+                    lineBuilder.append(" ");
+                    lineBuilder.append(condition.getOperator().getOperator());
+                    lineBuilder.append(" ?");
+                    lines.add(lineBuilder.toString());
+                    names.add(condition.getName());
+                    values.add(condition.getValue());
+                }
+                sqlBuilder.append(StringUtilities.join(lines, StringUtilities.surround(operator, " ")));
+                sqlWrapper.setNames(names.toArray(new String[names.size()]));
+                sqlWrapper.setValues(values.toArray(new Object[values.size()]));
             }
-            sqlBuilder.append(StringUtilities.join(lines, StringUtilities.surround(operator, " ")));
-            sqlWrapper.setNames(names.toArray(new String[names.size()]));
-            sqlWrapper.setValues(values.toArray(new Object[values.size()]));
         }
-        List<Sorter> _sorters = selectionGenerator.generateSorters(key, sorters);
-        if (CollectionUtilities.isNotEmpty(_sorters)) {
-            sqlBuilder.append(" ORDER BY ");
-            List<String> lines = new ArrayList<String>(_sorters.size());
-            for (Sorter sorter : _sorters) {
-                lines.add(sorter.getColumnName() + " " + sorter.getOperator().getOperator());
+        if (CollectionUtilities.isNotEmpty(sorters)) {
+            List<Sorter> _sorters = selectionGenerator.generateSorters(key, sorters);
+            if (CollectionUtilities.isNotEmpty(_sorters)) {
+                sqlBuilder.append(" ORDER BY ");
+                List<String> lines = new ArrayList<String>(_sorters.size());
+                for (Sorter sorter : _sorters) {
+                    lines.add(sorter.getColumnName() + " " + sorter.getOperator().getOperator());
+                }
+                sqlBuilder.append(StringUtilities.join(lines, StringUtilities.COMMA));
             }
-            sqlBuilder.append(StringUtilities.join(lines, StringUtilities.COMMA));
         }
         sqlWrapper.setSql(sqlBuilder.toString());
         return sqlWrapper;
