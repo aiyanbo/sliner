8441d06ed4e7190cad88af44dcd09163fabd1f82
Andy Ai <yanbo.ai@gmail.com>
2014-04-21 17:50:51 +0800

    Support "IN"

--- src/test/java/org/sliner/AppTest.java
+++ src/test/java/org/sliner/AppTest.java
@@ -23,8 +23,13 @@ public class AppTest extends TestCase {
         sorters.add("-level");
         sorters.add("age4Asc");
         SqlWrapper sqlWrapper = sqlLiner.wrap("search", conditions, "AND", sorters);
-        assertEquals("select * from tb_seller WHERE seller_name like ? AND seller_type = ? ORDER BY level DESC,age ASC",
-                sqlWrapper.getSql());
+        try {
+            assertEquals("select * from tb_seller WHERE seller_name like ? AND seller_type = ? ORDER BY level DESC,age ASC",
+                    sqlWrapper.getSql());
+        } catch (Exception e) {
+            assertEquals("select * from tb_seller WHERE seller_type = ? AND seller_name like ? ORDER BY level DESC,age ASC",
+                    sqlWrapper.getSql());
+        }
         System.out.println(sqlWrapper.getSql());
     }
 
