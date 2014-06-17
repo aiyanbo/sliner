ea14a9c3369530594b60f91ba9a826706332e77c
Andy Ai <yanbo.ai@gmail.com>
2014-06-17 11:56:12 +0800

    JPA implementation

--- src/main/java/org/sliner/mapper/impl/JpaSearchMapperImpl.java
+++ src/main/java/org/sliner/mapper/impl/JpaSearchMapperImpl.java
@@ -1,10 +1,14 @@
 package org.sliner.mapper.impl;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import org.sliner.mapper.ConditionMapping;
 import org.sliner.mapper.SearchMapper;
 import org.sliner.mapper.SorterMapping;
 
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Component:
@@ -14,6 +18,27 @@ import java.util.Set;
  * @author Andy Ai
  */
 public class JpaSearchMapperImpl implements SearchMapper {
+    private LoadingCache<String, SearchMapping> searchMappingCache;
+    private String basePackage;
+    private Long cacheInSeconds = 10L;
+
+    public JpaSearchMapperImpl() {
+        initComponent();
+    }
+
+    private void initComponent() {
+        searchMappingCache = CacheBuilder.newBuilder().
+                expireAfterWrite(cacheInSeconds, TimeUnit.SECONDS).
+                maximumSize(1024).
+                build(new CacheLoader<String, SearchMapping>() {
+                    @Override
+                    public SearchMapping load(String key) throws Exception {
+                        return null;
+                    }
+                });
+
+    }
+
     @Override
     public Set<String> getKeys() {
         return null;
@@ -43,4 +68,13 @@ public class JpaSearchMapperImpl implements SearchMapper {
     public Set<SorterMapping> getSorterMapper(String key) {
         return null;
     }
+
+    public void setBasePackage(String basePackage) {
+        this.basePackage = basePackage;
+    }
+
+    public void setCacheInSeconds(Long cacheInSeconds) {
+        this.cacheInSeconds = cacheInSeconds;
+    }
+
 }
