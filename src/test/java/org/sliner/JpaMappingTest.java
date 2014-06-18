e49f46d02dcfb2e57579a6c63165a6bdccfc7a83
Andy Ai <yanbo.ai@gmail.com>
2014-06-18 17:21:10 +0800

    Test cases

---
+++ src/test/java/org/sliner/JpaMappingTest.java
@@ -0,0 +1,52 @@
+package org.sliner;
+
+import junit.framework.TestCase;
+import org.sliner.domain.Seller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Unit test for simple App.
+ */
+public class JpaMappingTest extends TestCase {
+    private SqlLiner sqlLiner;
+
+    public void setUp() {
+        sqlLiner = SqlLinerBuilder.newBuilder().classes(Seller.class).build();
+    }
+
+    public void testWarp() {
+        Map<String, String> conditions = new HashMap<String, String>();
+        conditions.put("seller_name4Like2a", "Andy");
+        conditions.put("seller_type", "1204");
+        conditions.put("age4Gt", "23");
+        conditions.put("1", "1");// bad condition, it will be exclude the sql.
+        List<String> sorters = new ArrayList<String>();
+        sorters.add("count4Desc");//bad sort, it will be exclude the sql.
+        sorters.add("-level");
+        sorters.add("age4Asc");
+        SqlWrapper sqlWrapper = sqlLiner.wrap("seller", conditions, "AND", sorters);
+        System.out.println(sqlWrapper.getSql());
+    }
+
+    public void testWarpIdentifier() {
+        SqlWrapper sqlWrapper = sqlLiner.wrapIdentifier("seller", "0000");
+        assertEquals("select * from tb_seller WHERE seller_id = ?", sqlWrapper.getSql());
+        assertEquals("0000", sqlWrapper.getValues()[0]);
+    }
+
+    public void testMultiValues() {
+        Map<String, String> conditions = new HashMap<String, String>();
+        conditions.put("seller_name4In", "Andy, Lily");
+        conditions.put("seller_type", "1204");
+        SqlWrapper sqlWrapper = sqlLiner.wrap("seller", conditions, "AND", null);
+        System.out.println(sqlWrapper.getSql());
+        for (Object value : sqlWrapper.getValues()) {
+            System.out.println(value);
+        }
+    }
+
+}
---
+++ src/test/java/org/sliner/domain/Seller.java
@@ -0,0 +1,78 @@
+package org.sliner.domain;
+
+import org.sliner.annotation.Criteria;
+import org.sliner.annotation.Managed;
+import org.sliner.annotation.Sorting;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * Component:
+ * Description:
+ * Date: 14-6-18
+ *
+ * @author Andy Ai
+ */
+@Managed(key = "seller")
+@Entity(name = "tb_seller")
+public class Seller {
+    @Id
+    @Column(name = "seller_id")
+    private String sellerId;
+
+    private String sellerName;
+    @Criteria
+    @Column(name = "seller_type")
+    private Long sellerType;
+    @Sorting
+    @Column(name = "age")
+    private int age;
+
+    private int level;
+
+    public String getSellerId() {
+        return sellerId;
+    }
+
+    public void setSellerId(String sellerId) {
+        this.sellerId = sellerId;
+    }
+
+    @Criteria(multiple = true)
+    @Column(name = "seller_name")
+    public String getSellerName() {
+        return sellerName;
+    }
+
+    public void setSellerName(String sellerName) {
+        this.sellerName = sellerName;
+    }
+
+    public Long getSellerType() {
+        return sellerType;
+    }
+
+    public void setSellerType(Long sellerType) {
+        this.sellerType = sellerType;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Sorting
+    @Column(name = "level")
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+}
